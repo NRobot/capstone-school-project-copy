679f7fdbf6f488db8ea44c93f04f7edac3bdc9dc
zxeleven <john7233@gmail.com>
2019-10-03 17:23:05 -0500

    Model for searching by genre

---
+++ MyTVStreamingService/Models/ShowGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MyTVStreamingService.Models
+{
+    public class ShowGenreViewModel
+    {
+        public List<Show> Shows { get; set; }
+        public SelectList Genres { get; set; }
+        public string ShowGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
