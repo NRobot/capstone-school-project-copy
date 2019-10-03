9a61d0c273dabb02e953d4debb164fadd871d204
zxeleven <john7233@gmail.com>
2019-10-03 17:58:16 -0500

    Added context for shows to the database

--- MyTVStreamingService/Startup.cs
+++ MyTVStreamingService/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MyTVStreamingService.Data;
 
 namespace MyTVStreamingService
 {
@@ -24,6 +26,10 @@ namespace MyTVStreamingService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<MyTVContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MyTVContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
