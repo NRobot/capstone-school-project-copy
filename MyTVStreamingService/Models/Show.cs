60a501e50aa925002bda2d93eb08ac0f381d050f
zxeleven <john7233@gmail.com>
2019-10-03 17:22:39 -0500

    Model with attributes for a show

---
+++ MyTVStreamingService/Models/Show.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTVStreamingService.Models
+{
+    public class Show
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
+        public String Title { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+        public String Genre { get; set; }
+
+        [Display(Name = "Number of Seasons")]
+        public int NumberOfSeasons { get; set; }
+
+        [Range(1, 10), Required]
+        public double Rating { get; set; }
+    }
+}
