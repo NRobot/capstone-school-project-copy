c92f7fe83597ae76c7086e18990093ec4fd941f7
zxeleven <john7233@gmail.com>
2019-10-24 20:04:44 -0500

    Fixed mix of tabs and spaces

--- MyTVStreamingService/Models/User.cs
+++ MyTVStreamingService/Models/User.cs
@@ -8,28 +8,28 @@ using System.Threading.Tasks;
 
 namespace MyTVStreamingService.Models
 {
-    public class User
-    {
-        public int Id { get; set; }
+	public class User
+	{
+		public int Id { get; set; }
 
-        [Display(Name = "Username"), StringLength(60, MinimumLength = 3), Required]
-        public String userName { get; set; }
+		[Display(Name = "Username"), StringLength(60, MinimumLength = 3), Required]
+		public String userName { get; set; }
 
-        [Display(Name = "Password"), StringLength(60, MinimumLength = 9), Required]
-        public String userPassword { get; set; }
+		[Display(Name = "Password"), StringLength(60, MinimumLength = 9), Required]
+		public String userPassword { get; set; }
 		
 		[Display(Name = "First Name"), StringLength(60, MinimumLength = 3), Required]
-        public String firstName { get; set; }
+		public String firstName { get; set; }
 		
 		[Display(Name = "Last Name"), StringLength(60, MinimumLength = 3), Required]
-        public String lastName { get; set; }
+		public String lastName { get; set; }
 
 		[Display(Name = "Email"), StringLength(60, MinimumLength = 3), Required]
-        public String emailAddress { get; set; }
+		public String emailAddress { get; set; }
 
-        [Display(Name = "Account Creation Date"), DataType(DataType.Date)]
-        public DateTime accCreationDate { get; set; }
+		[Display(Name = "Account Creation Date"), DataType(DataType.Date)]
+		public DateTime accCreationDate { get; set; }
 
-        
-    }
+		
+	}
 }
