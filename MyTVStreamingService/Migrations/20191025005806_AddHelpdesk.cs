15b3a64ef686dcc6335fcdae61088a6a1ea273af
zxeleven <john7233@gmail.com>
2019-10-24 20:06:12 -0500

    Migrated Helpdesk to the database

---
+++ MyTVStreamingService/Migrations/20191025005806_AddHelpdesk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyTVStreamingService.Migrations
+{
+    public partial class AddHelpdesk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdminHelpdesk",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(maxLength: 60, nullable: false),
+                    Username = table.Column<string>(maxLength: 60, nullable: true),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    Message = table.Column<string>(nullable: false),
+                    ArrivalTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminHelpdesk", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminHelpdesk");
+        }
+    }
+}
