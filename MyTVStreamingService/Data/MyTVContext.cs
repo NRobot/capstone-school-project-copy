b732d0036c3e65bf6e5a354eefcc8699fe64f28d
zxeleven <john7233@gmail.com>
2019-11-10 23:05:34 -0600

    Ensure identity table is created in database

--- MyTVStreamingService/Data/MyTVContext.cs
+++ MyTVStreamingService/Data/MyTVContext.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using MyTVStreamingService.Models;
-
 
 namespace MyTVStreamingService.Data
 {
-    public class MyTVContext : IdentityDbContext<MyTVUser, MyTVRole, int>
+    public class MyTVContext : IdentityDbContext<MyTVUser>
     {
         public MyTVContext(DbContextOptions<MyTVContext> options)
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyTVUser>().ToTable("AspNetUsers");
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Show> Show { get; set; }
         public DbSet<ShowService> ShowService{get;set;}
         public DbSet<Recommendation> Recommendation { get; set; }
         public DbSet<Service> Service { get; set; }
-       // public DbSet<User> User { get; set; }
 
         public DbSet<AdminHelpdesk> AdminHelpdesk { get; set; }
 
