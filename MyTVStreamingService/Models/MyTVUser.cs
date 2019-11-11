55fcacc9db5cebb29b7dff42e5b19a7d4c11dae1
zxeleven <john7233@gmail.com>
2019-11-10 23:06:53 -0600

    Re-added MyTVUser

--- MyTVStreamingService/Models/MyTVUser.cs
+++ MyTVStreamingService/Models/MyTVUser.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,25 +7,18 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MyTVStreamingService.Models
 {
-    public class MyTVUser : IdentityUser<int>
+    public class MyTVUser : IdentityUser
     {
-        //[Display(Name = "UserName"), StringLength(60, MinimumLength = 3), Required]
-		// public String UserName { get; set; }
 
-		//[Display(Name = "Password"), StringLength(60, MinimumLength = 9), Required]
-		//public String UserPassword { get; set; }
-		
-		[Display(Name = "First Name"), StringLength(60, MinimumLength = 3)]//, Required]
-		public String FirstName { get; set; }
-		
-		[Display(Name = "Last Name"), StringLength(60, MinimumLength = 3)]//, Required]
-		public String LastName { get; set; }
+        [Display(Name = "First Name"), StringLength(60, MinimumLength = 3)]//, Required]
+        public String FirstName { get; set; }
 
-		//[Display(Name = "Email"), StringLength(60, MinimumLength = 3), Required]
-		//public String EmailAddress { get; set; }
+        [Display(Name = "Last Name"), StringLength(60, MinimumLength = 3)]//, Required]
+        public String LastName { get; set; }
 
-		[Display(Name = "Account Creation Date"), DataType(DataType.Date)]
-		public DateTime AccCreationDate { get; set; }
+
+        [Display(Name = "Account Creation Date"), DataType(DataType.Date)]
+        public DateTime AccCreationDate { get; set; }
 
     }
 }
