cc6194e9644aa6e6fd1a74c544427f448b05c338
eChadwick <en.chadwick@gmail.com>
2019-11-20 13:08:14 -0600

    remove unused methods from RecommendationsController

--- MyTVStreamingService/Controllers/RecommendationsController.cs
+++ MyTVStreamingService/Controllers/RecommendationsController.cs
@@ -21,52 +21,6 @@ namespace MyTVStreamingService.Controllers
             _context = context;
         }
 
-        // GET: Recommendations
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Recommendation.ToListAsync());
-        }
-
-        // GET: Recommendations/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var recommendation = await _context.Recommendation
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (recommendation == null)
-            {
-                return NotFound();
-            }
-
-            return View(recommendation);
-        }
-
-        // GET: Recommendations/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Recommendations/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public async Task<IActionResult> Create([Bind("Id,Name,QueryString")] Recommendation recommendation)
-        // {
-        //     if (ModelState.IsValid)
-        //     {
-        //         _context.Add(recommendation);
-        //         await _context.SaveChangesAsync();
-        //         return RedirectToAction(nameof(Index));
-        //     }
-        //     return View(recommendation);
-        // }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
@@ -114,86 +68,6 @@ namespace MyTVStreamingService.Controllers
                                 where showIds.Contains(show.Id)
                                 select show).ToList();
             return View("Recommendation");
-        }
-
-        // GET: Recommendations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var recommendation = await _context.Recommendation.FindAsync(id);
-            if (recommendation == null)
-            {
-                return NotFound();
-            }
-            return View(recommendation);
-        }
-
-        // POST: Recommendations/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,QueryString")] Recommendation recommendation)
-        {
-            if (id != recommendation.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(recommendation);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RecommendationExists(recommendation.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(recommendation);
-        }
-
-        // GET: Recommendations/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var recommendation = await _context.Recommendation
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (recommendation == null)
-            {
-                return NotFound();
-            }
-
-            return View(recommendation);
-        }
-
-        // POST: Recommendations/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var recommendation = await _context.Recommendation.FindAsync(id);
-            _context.Recommendation.Remove(recommendation);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool RecommendationExists(int id)
